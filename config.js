09c5afe3c0bc220940d2d0034b0e8c6a8a04def6
Khai17122002 <96589152+Khai17122002@users.noreply.github.com>
2021-12-26 23:19:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "LInh dễ thương With me",
-    introTitle: 'LInh <3',
+    titleWeb: "Gửi con lợn má bánh bao",
+    introTitle: 'Bánh bao hình lợn',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Bớt cute đeeee',
+    desc: 'Bạn thích mời tôi đi ăn à',
+    btnYes: 'thích lắm chứ<33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ nụ cười nhưng với a nụ cười của em là đẹp nhất <3333',
-    btnReply: 'Gửi cho anh <3',
+    question: 'Lần sau nói chuyện với nta thì đừng có tắt máy',
+    btnReply: 'Gửi cho Khải đẹp trai <3',
     reply: '',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Đi học OFF mình dẫn công chúa đi chơi nha :))',
+    messDesc: 'Đi học bánh bao dẫn t đi ăn ốc nhé',
     btnAccept: 'mình đồng ý',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
